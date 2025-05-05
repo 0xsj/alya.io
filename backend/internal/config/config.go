9a97d89807b3dba4412ed22c5e1bc24dcf41acb1
0xsj <sjtommylee@gmail.com>
2025-05-05 12:39:04 -0700

    config.go enhanced

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"strings"
 	"time"
 
 	"github.com/0xsj/alya.io/backend/pkg/logger"
@@ -61,4 +62,51 @@ type CorsConfig struct {
 	AllowedHeaders  []string
 	AllowCredentials bool
 	MaxAge          int
+}
+
+func Load() {}
+
+func validate() error {
+
+	return nil
+}
+
+func getLogLevel(level string) int {
+	switch strings.ToLower(level) {
+	case "debug":
+		return logger.DebugLevel
+	case "info":
+		return logger.InfoLevel
+	case "warn", "warning":
+		return logger.WarnLevel
+	case "error":
+		return logger.ErrorLevel
+	case "fatal":
+		return logger.FatalLevel
+	case "panic":
+		return logger.PanicLevel
+	default:
+		return logger.InfoLevel
+	}
+}
+
+func StringMap() {}
+
+func getLogLevelName(level int) string {
+	switch level {
+	case logger.DebugLevel:
+		return "debug"
+	case logger.InfoLevel:
+		return "info"
+	case logger.WarnLevel:
+		return "warn"
+	case logger.ErrorLevel:
+		return "error"
+	case logger.FatalLevel:
+		return "fatal"
+	case logger.PanicLevel:
+		return "panic"
+	default:
+		return "unknown"
+	}
 }

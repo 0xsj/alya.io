3b23a832cb43707dae1a14f2c6846b004d128e9d
0xsj <sjtommylee@gmail.com>
2025-05-05 12:36:11 -0700

    env.go structs

--- backend/internal/config/env.go
+++ backend/internal/config/env.go
@@ -1,1 +1,17 @@
 package config
+
+import "time"
+
+type EnvProvider interface {
+	Get(key string)	string
+	GetDefault(key, defaultValue string) string
+	GetBool(key string) (bool, error)
+	GetBoolDefault(key string, defaultValue bool) bool
+	GetInt(key string) (int error)
+	GetIntDefault(key string, defaultValue int) int
+	GetDuration(key string) (time.Duration, error)
+	GetDurationDefault(key string, defaultValue time.Duration) time.Duration
+	GetArray(key string) []string
+}
+
+type OsEnvProvider struct {}
